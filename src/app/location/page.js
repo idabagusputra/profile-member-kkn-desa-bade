4aaac12b483d43edb23468d77a3983db2f1eb716
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2023-12-23 23:03:27 +0700

    update location page

--- src/app/location/page.js
+++ src/app/location/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Header from "../components/header.js";
 import Footer from "../components/footer.js";
-import Landing from "../components/landing.js";
+import Location from "../components/location.js";
 export default function Home() {
   return (
     <div>
       <Header />
-      <Landing />
+      <Location />
       <Footer />
     </div>
   );
